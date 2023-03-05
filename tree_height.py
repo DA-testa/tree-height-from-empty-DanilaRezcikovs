acf6c2f01d7d739a71aeac4a88e14941e42f2784
DanilaRezcikovs <125744863+DanilaRezcikovs@users.noreply.github.com>
2023-03-05 21:59:46 +0000

    commit

--- tree_height.py
+++ tree_height.py
@@ -7,31 +7,44 @@ import re
 
 
 class Node:
-    def __init__(self, value):
-        self.value = value
+    def __init__(self, index):
+        self.index = index
         self.children = []
-        self.parent = None
 
-n_str = input().strip()
-n = int(n_str)
-parent_indices_str = input().strip()
-parent_indices = list(map(int, parent_indices_str.split()))
-nodes = [Node(i) for i in range(n)]
+def compute_height(nodes):
+    root = None
+    for i, parent_index in enumerate(nodes):
+        if parent_index == -1:
+            root = i
+        else:
+            nodes[parent_index].children.append(nodes[i])
+    return get_height(root)
 
-for i in range(n):
-    parent_index = parent_indices[i]
-    if parent_index == -1:
-        root = nodes[i]
-    else:
-        parent_node = nodes[parent_index]
-        parent_node.children.append(nodes[i])
-        nodes[i].parent = parent_node
-
-def max_depth(node):
+def get_height(node):
     if not node.children:
         return 1
-    else:
-        return 1 + max(max_depth(child) for child in node.children)
+    return 1 + max([get_height(child) for child in node.children])
 
-height = max_depth(root)
-print(height)
+if __name__ == '__main__':
+    import os
+
+    input_type = input("Input data from keyboard (I) or file (F)? ").upper()
+    while input_type not in ['I', 'F']:
+        print("Invalid input. Please enter I or F.")
+        input_type = input("Input data from keyboard (I) or file (F)? ").upper()
+
+    if input_type == 'I':
+        n = int(input("Enter the number of nodes: "))
+        parents = list(map(int, input("Enter the list of parents for each node (-1 for root): ").split()))
+    else:
+        file_name = input("Enter the file name (without letter 'a'): ")
+        while 'a' in file_name:
+            print("Invalid file name. Please enter a file name without the letter 'a'.")
+            file_name = input("Enter the file name (without letter 'a'): ")
+        file_path = os.path.join('folder', file_name)
+        with open(file_path, 'r') as f:
+            n = int(f.readline())
+            parents = list(map(int, f.readline().split()))
+
+    nodes = [Node(i) for i in range(n)]
+    print(compute_height(nodes))
