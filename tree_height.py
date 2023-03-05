bfa8bc1ce4e22ae39cc13f242d60f6e99638b7ec
DanilaRezcikovs <125744863+DanilaRezcikovs@users.noreply.github.com>
2023-03-05 18:46:22 +0000

    commit

--- tree_height.py
+++ tree_height.py
@@ -11,7 +11,7 @@ class Node:
         self.children = []
         self.parent = None
 
-n = int(float(input().strip()))
+n = int(input().strip())
 parent_indices = list(map(int, input().strip().split()))
 
 nodes = [Node(i) for i in range(n)]
